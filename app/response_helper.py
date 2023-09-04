921d01bedcc484dad085c97e77fc934e304c5eaf
Michal Jozwiak <mjozwiak@bink.com>
2023-09-04 14:16:39 +0100

    add error response for slim chickens

--- app/response_helper.py
+++ app/response_helper.py
@@ -22,6 +22,16 @@ def _get_itsu_response(response_body: dict) -> str | dict:
         return response_body
 
 
+def _get_slim_chickens_response(response_body: dict) -> str | dict:
+    try:
+        if error := response_body.get("errorMessage"):
+            return error
+        else:
+            return response_body["error"]["description"]
+    except (KeyError, TypeError):
+        return response_body
+
+
 def _get_the_works_response(response_body: dict) -> str | dict:
     try:
         return response_body["result"][1]
@@ -34,6 +44,7 @@ RESPONSE_METHODS = {
     "wasabi-club": _get_wasabi_response,
     "the-works": _get_the_works_response,
     "itsu": _get_itsu_response,
+    "slim-chickens": _get_slim_chickens_response
 }
 
 
